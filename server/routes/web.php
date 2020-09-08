ba40c2e7f65eaf06a0f2e3b45539a1c2a7ce8ff3
noopy0415 <noopy0415@gmail.com>
2020-09-08 17:27:41 +0900

    modify Route

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'RestaurantController@index');
+Route::resource('restaurants', 'RestaurantController', ['only' => ['index', 'show']]);;
