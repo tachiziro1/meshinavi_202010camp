1daa0c562df3f8829bddf185c7718ed02bfc593b
noopy0415 <noopy0415@gmail.com>
2020-09-08 17:21:36 +0900

    modify RestaurantController

--- server/app/Http/Controllers/RestaurantController.php
+++ server/app/Http/Controllers/RestaurantController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class RestaurantController extends Controller
 {
-    //
+    public function index()
+    {
+        $restaurants = Restaurant::all();
+        return view('restaurants.index', ['restaurants' => $restaurants]);
+    }
+
+    public function show($id)
+    {
+        $restaurant = Restaurant::find($id);
+        return view('restaurants.show',['restaurant' => $restaurant]);
+    }
 }
